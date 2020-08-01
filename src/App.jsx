59b4860ab902d1c20ed5005f7521d4e9b030430e
velveet <velveet@pm.me>
2020-08-01 14:37:12 -0500

    Installed react-bootstrap through npm. Styling the application with react-bootstrap.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Clock from './Clock';
 import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
 
@@ -28,10 +29,10 @@ class App extends Component {
 
         <Clock deadline={this.state.deadline} />
 
-        <div>
-          <input placeholder="new date" onChange={event => this.setState({newDeadline: event.target.value})} id="inputBox" />
-          <button onClick={() => this.changeDeadline()}>Submit</button>
-        </div>
+        <Form inline>
+          <FormControl className="Deadline-input" placeholder="new date" onChange={event => this.setState({newDeadline: event.target.value})} id="inputBox" />
+          <Button onClick={() => this.changeDeadline()}>Submit</Button>
+        </Form>
 
       </div>
     )
