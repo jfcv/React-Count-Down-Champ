a13b1c7e298ba38192bfe4f918431a3356b4374c
velveet <velveet@pm.me>
2020-07-31 21:00:42 -0500

    'newDeadline' property on the this.state object. Updating countdown date with this.setState() method using the 'deadline' property that belongs to the this.state object

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    //input DOM element
+    this.setState({deadline: this.state.newDeadline});
+    let inputBox = document.getElementById('inputBox');
+    inputBox.value = '';
+  }
+
   render() {
     return (
-      <div>
-        <div>Countdown to December 25, 2020</div>
+
+      <div className="App">
+
+        <div className="App-title">Countdown to {this.state.deadline}</div>
+
         <div>
-          <div>14 days</div>
-          <div>30 hours</div>
-          <div>15 minutes</div>
-          <div>20 seconds</div>
+          <div className="Clock-days">14 days </div>
+          <div className="Clock-hours">30 hours </div>
+          <div className="Clock-minutes">15 minutes </div>
+          <div className="Clock-seconds">20 seconds</div>
         </div>
-        <input placeholder="new date" />
-        <button>Submit</button>
+
+        <div>
+          <input placeholder="new date" onChange={event => this.setState({newDeadline: event.target.value})} id="inputBox" />
+          <button onClick={() => this.changeDeadline()}>Submit</button>
+        </div>
+
       </div>
     )
   }
